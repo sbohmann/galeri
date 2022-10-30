157fb794f55e00c99b07845c9ffcb0ed4aba6478
sbohmann <mail@sebastian-bohmann.com>
2022-10-30 14:12:51 +0100

    galeri view

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+let fs = require('fs')
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { images: readImages() });
 });
+
+function readImages() {
+  return fs.readdirSync('public/images')
+}
 
 module.exports = router;
