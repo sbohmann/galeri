798c60105a5db58c0ec6f3689c8f8d1108fc3048
sbohmann <mail@sebastian-bohmann.com>
2023-06-08 19:02:36 +0200

    login

--- app.js
+++ app.js
@@ -1,26 +1,49 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+const session = require('express-session')
+const indexRouter = require('./routes/index')
+const uploadRouter = require('./routes/upload')
+const loginRouter = require('./routes/login')
+const fs = require('fs')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
+const app = express()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
+app.use(session({
+  secret: fs.readFileSync('config/secret.txt', 'utf8')
+}))
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/login', loginRouter);
+
+app.use(function (request, response, next) {
+  let userId = request.session.userId
+  if (userId === undefined) {
+    if (request.method === 'GET') {
+      response.status(302)
+      response.set('Location', '/login')
+      response.send()
+    } else {
+      response.status(401)
+      response.send()
+    }
+  } else {
+    next()
+  }
+})
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/upload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -29,6 +52,7 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
+  console.log("Error:", err)
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
