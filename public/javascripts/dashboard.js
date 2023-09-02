14c557c20ccf6e1b61a49898dea7289fdbd609b0
sbohmann <github@yeoman.at>
2023-09-02 12:53:07 +0200

    Dashboard

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -1,14 +1,47 @@
 window.onload = initialize
 
 function initialize() {
+    let systemsView = document.getElementById('systems')
     let timeLabel = document.getElementById('timelabel')
-    let timeLabelText = document.createTextNode("...")
-    timeLabel.appendChild(timeLabelText)
+    timeLabel.textContent = '...'
 
     function update() {
         fetch('/dashboard/time')
             .then(response => response.text()
-                .then(text => timeLabelText.textContent = text))
+                .then(text => timeLabel.textContent = text))
+        fetch('/dashboard/systems')
+            .then(response => response.json()
+                .then(systems => {
+                    for (let systemName of Object.keys(systems)) {
+                        let system = systems[systemName]
+                        let systemViewId = 'system_' + systemName; //TODO normalize system name
+                        let systemStatusLabelId = 'system_' + systemName + '_status';
+                        let systemView = document.getElementById(systemViewId)
+                        let systemStatusLabel
+                        if (systemView) {
+                            systemStatusLabel = document.getElementById(systemStatusLabelId)
+                        } else {
+                            let systemsView = document.getElementById('systems')
+                            systemView = document.createElement('div')
+                            systemView.id = systemViewId
+                            let systemNameLabel = document.createElement('h3')
+                            systemNameLabel.textContent = systemName
+                            systemView.appendChild(systemNameLabel)
+                            systemStatusLabel = document.createElement('h1')
+                            systemStatusLabel.id = systemStatusLabelId
+                            systemView.appendChild(systemStatusLabel)
+                            systemsView.appendChild(systemView)
+                        }
+                        systemStatusLabel.textContent = system.status
+                        if (system.status === 'OK') {
+                            systemStatusLabel.classList.remove('red')
+                            systemStatusLabel.classList.add('green')
+                        } else {
+                            systemStatusLabel.classList.remove('green')
+                            systemStatusLabel.classList.add('red')
+                        }
+                    }
+                }))
     }
 
     setInterval(update, 5000)
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -6,30 +6,21 @@ let router = express.Router()
 router.get('/', function (request, response) {
     response.render('dashboard', {
         title: "Dashboard",
-        scripts: ["dashboard.js"],
-        systems: ['alamera']
+        scripts: ["dashboard.js"]
     });
 })
 
-router.get('/system/:system/state', function (request, response) {
-    if (request.params.system) {
-        response.send("OK");
-    } else {
-        response.send("unknown");
-    }
+router.get('/systems', function (request, response) {
+    response.send({
+        "alamera": {
+            status: 'OK',
+            statusDate: LocalTime.now()
+        }
+    })
 })
 
 router.get('/time', function (request, response) {
     response.send(LocalTime.now())
 })
-
-function readImages() {
-    let imageDirectory = 'public/images/c9'
-    if (fs.existsSync(imageDirectory)) {
-        return fs.readdirSync(imageDirectory)
-    } else {
-        return []
-    }
-}
 
 module.exports = router
