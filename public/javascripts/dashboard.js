e1aef8b20d10c6a6755ab02fe753b52456c3a4ec
sbohmann <github@yeoman.at>
2023-09-02 18:24:23 +0200

    Dashboard

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -16,12 +16,14 @@ function initialize() {
                         let system = systems[systemName]
                         let systemViewId = 'system_' + systemName; //TODO normalize system name
                         let systemStatusLabelId = 'system_' + systemName + '_status';
+                        let systemStatusUpdatedLabelId = 'system_' + systemName + '_status_update';
                         let systemView = document.getElementById(systemViewId)
                         let systemStatusLabel
+                        let systemStatusUpdatedLabel
                         if (systemView) {
                             systemStatusLabel = document.getElementById(systemStatusLabelId)
+                            systemStatusUpdatedLabel = document.getElementById(systemStatusUpdatedLabelId)
                         } else {
-                            let systemsView = document.getElementById('systems')
                             systemView = document.createElement('div')
                             systemView.id = systemViewId
                             let systemNameLabel = document.createElement('h3')
@@ -30,16 +32,20 @@ function initialize() {
                             systemStatusLabel = document.createElement('h1')
                             systemStatusLabel.id = systemStatusLabelId
                             systemView.appendChild(systemStatusLabel)
+                            systemStatusUpdatedLabel = document.createElement('h3')
+                            systemStatusUpdatedLabel.id = systemStatusUpdatedLabelId
+                            systemView.appendChild(systemStatusUpdatedLabel)
                             systemsView.appendChild(systemView)
                         }
-                        systemStatusLabel.textContent = system.status
-                        if (system.status === 'OK') {
+                        systemStatusLabel.textContent = system.state
+                        if (system.state === 'OK') {
                             systemStatusLabel.classList.remove('red')
                             systemStatusLabel.classList.add('green')
                         } else {
                             systemStatusLabel.classList.remove('green')
                             systemStatusLabel.classList.add('red')
                         }
+                        systemStatusUpdatedLabel.textContent = system.updated
                     }
                 }))
     }
--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,7 +1,23 @@
+const fs = require("fs");
 const {LocalTime} = require('js-joda')
 
 let express = require('express')
 let router = express.Router()
+
+let tokenForSystem = new Map()
+
+Object.entries(JSON.parse(fs.readFileSync('config/systemTokens.json', 'utf8')))
+    .map(entry => {
+        let [system, token] = entry
+        tokenForSystem.set(system, token)
+    })
+
+let stateForSystem = {}
+
+function tokenIsCorrect(systemName, token) {
+    let correctToken = tokenForSystem.get(systemName)
+    return (correctToken && correctToken.length > 0 && token === correctToken)
+}
 
 router.get('/', function (request, response) {
     response.render('dashboard', {
@@ -11,12 +27,25 @@ router.get('/', function (request, response) {
 })
 
 router.get('/systems', function (request, response) {
-    response.send({
-        "alamera": {
-            status: 'OK',
-            statusDate: LocalTime.now()
-        }
-    })
+    console.log(stateForSystem)
+    response.send(stateForSystem)
+})
+
+router.put('/system/:name/report', function (request, response) {
+    let systemName = request.params.name
+    let token = request.query.token
+    if (!tokenIsCorrect(systemName, token)) {
+        response.status(401)
+        response.send()
+        return
+    }
+    let message = request.body
+    stateForSystem[systemName] = {
+        state: message.state,
+        updated: LocalTime.now(),
+        message: message.message
+    }
+    response.send()
 })
 
 router.get('/time', function (request, response) {
