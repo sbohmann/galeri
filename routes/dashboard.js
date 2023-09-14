49d1b307060c1c0b1a01c709f6a4db665bcbbcef
sbohmann <github@yeoman.at>
2023-09-14 19:09:26 +0200

    Notifications

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -79,7 +79,7 @@ function checkStates() {
     for (let systemName in stateForSystem) {
         let state = stateForSystem[systemName]
         console.log(state.state)
-        if (Instant.now().isAfter(state.updateInstant.plusMinutes(1))) {
+        if (Instant.now().isAfter(state.updateInstant.plusSeconds(60))) {
             notify("System [" + systemName + "] state older than 1 minute")
         }
     }
