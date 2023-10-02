7b6525951a31852ba7482e5f4ade37866e53cdc0
sbohmann <github@yeoman.at>
2023-10-02 19:20:34 +0200

    Wiesn 23

--- routes/wiesn23.js
+++ routes/wiesn23.js
@@ -1,18 +1,40 @@
 let express = require('express')
 let router = express.Router()
 let fs = require('fs')
+const path = require("path");
+const videoSuffices = ['mov', 'mp4', 'webm']
+    .map(suffix => new RegExp('\\.' + suffix + '$'))
 
 router.get('/', function (request, response) {
-    response.render('wiesn23', {images: readImages()});
+    response.render('wiesn23', {images: readImages()})
 })
 
 function readImages() {
     let imageDirectory = 'public/images/wiesn23'
     if (fs.existsSync(imageDirectory)) {
         return fs.readdirSync(imageDirectory)
+            .flatMap(filename => {
+                let stat = fs.statSync(path.join(imageDirectory, filename))
+                if (stat.isFile) {
+                    return [{
+                        filename,
+                        video: isVideo(filename)
+                    }]
+                }
+                return []
+            })
     } else {
         return []
     }
+}
+
+function isVideo(filename) {
+    for (let suffix of videoSuffices) {
+        if (filename.match(suffix)) {
+            return true
+        }
+    }
+    return false;
 }
 
 module.exports = router
