c4a55d262ed6eecee03061e24e3ea7e4aa73560c
sbohmann <github@yeoman.at>
2024-03-17 11:08:32 +0100

    Fixed login redirect

---
+++ public/javascripts/notes.js
@@ -0,0 +1,7 @@
+window.onload = init
+
+function init() {
+    document
+        .getElementById("text-input")
+        .focus()
+}
--- routes/notes.js
+++ routes/notes.js
@@ -2,11 +2,33 @@ let express = require('express')
 
 let router = express.Router()
 
+let notes = []
+
 router.get('/', function (request, response) {
-    response.render('clock', {
+    response.render('notes', {
         title: "Notes",
-        scripts: ["notes.js"]
+        scripts: ["notes.js"],
+        notes
     });
 })
+
+router.post('/post', function (request, response) {
+    withText(
+        request.body.text,
+        text => notes.unshift(text))
+    response.status(302)
+    response.set('Location', '/notes')
+    response.send()
+})
+
+function withText(value, action) {
+    if (value === undefined) {
+        return
+    }
+    value = value.trim()
+    if (value.length > 0) {
+        action(value)
+    }
+}
 
 module.exports = router
